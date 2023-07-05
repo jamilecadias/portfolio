d0b6de9046aded402a9115fd0382e56338e17bcd
jamilecadias <jamilecadias@gmail.com>
2023-07-04 23:01:01 -0300

    Editing project texts

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,26 +8,26 @@ import project4Image from 'assets/img/project4.png';
 
 const projects = [
   {
-    title: 'Coffea Shop ecommerce',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Coffea Shop E-commerce',
+    description: 'E-commerce of a specialty coffee shop with CRUD-based user and product management system and Mysql database. Final project in a full stack course team.',
     link: 'https://github.com/jamilecadias/ecommerce_coffeaShop',
     image: project1Image,
   },
   {
     title: 'Espacio Zen',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    description: 'Webpage to manage the registration of students to different Yoga courses, with CRUD system of users and courses. Individual final project in Angular for Angular course.',
     link: 'https://github.com/jamilecadias/yoga-angular-project',
     image: project2Image,
   },
   {
-    title: 'Collaborative storytelling page',
-    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    title: 'Collaborative storytelling Blog',
+    description: 'Collaborative storytelling publishing blog made in Python and Django. Final individual project for Python course.',
     link: 'https://github.com/jamilecadias/proyecto_python',
     image: project3Image,
   },
   {
     title: 'Conexão Brasil ',
-    description: 'Dashboard to manage users of a language course with a CRUD system in Node.js, using APIs.',
+    description: 'Landing page and user self-management dashboard for a language course, with a CRUD system in Node.js, using APIs and database in Mysql. Final individual project for the full stack course.',
     link: 'https://github.com/jamilecadias/proyecto_python',
     image: project4Image,
   },
